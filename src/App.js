665540a7fb8fd0804b699ddc9537b733850ccf74
lulo93 <94081621+lulo93@users.noreply.github.com>
2022-01-31 11:35:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { SpinnerCircular } from "spinners-react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-        <SpinnerCircular />
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div className="App"></div>;
 }
-
-export default App;
