eb4449694fd07e6506f6c3b034889fb4ebd9f7ae
lulo93 <94081621+lulo93@users.noreply.github.com>
2022-02-02 22:47:17 +0000

    Made small adjustments to css and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "./index.css";
+import React from "react";
 
 export default function App() {
   return <div className="App"></div>;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
-
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <div className="credits">
-        <small>
-          <a
-            href="https://github.com/lulo93/weather-react-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open-source code
-          </a>
-          {""} by Luisa Rua Estrada
-        </small>
+      <div className="container">
+        <Weather />
+        <div className="credits">
+          <small>
+            <a
+              href="https://github.com/lulo93/weather-react-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open-source code
+            </a>
+            {""} by Luisa Rua Estrada
+          </small>
+        </div>
       </div>
     </div>
   );
